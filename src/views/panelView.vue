4583dd441bd7cfd33f4789855d96da7ce8f7b1c3
Alvaro Marroquin <alvarochavas1994@gmail.com>
2025-01-19 18:44:09 +0100

    Delete functionality working

--- src/views/panelView.vue
+++ src/views/panelView.vue
@@ -5,16 +5,16 @@
         <table>
             <thead>
                 <tr>
-                <th>#ID</th>
-                <th>NOMBRE</th>
-                <th>NUMERO</th>
-                <th>EMAIL</th>
-                <th>CONTRASEÑA</th>
-                <th>ACCIONES</th>
-            </tr>
+                    <th>#ID</th>
+                    <th>NOMBRE</th>
+                    <th>NUMERO</th>
+                    <th>EMAIL</th>
+                    <th>CONTRASEÑA</th>
+                    <th>ACCIONES</th>
+                </tr>
             </thead>
             <!-- Here v-for to iterate elements (users) -->
-             <tbody>
+            <tbody>
                 <tr v-for="user in users" :key="user.id">
                     <td>{{ user._id }}</td>
                     <td>{{ user.name }}</td>
@@ -23,44 +23,108 @@
                     <td>{{ user.password }}</td>
                     <td>
                         <button>Editar</button>
-                        <button>Eliminar</button>
+                        <button @click="deleteUser(user._id)">Eliminar</button>
                     </td>
                 </tr>
-             </tbody>
-             <tfoot>
+            </tbody>
+            <tfoot>
                 <tr>
                     <td colspan="6">Usuarios desde Mongo Atlas</td>
                 </tr>
-             </tfoot>
+            </tfoot>
         </table>
         <div class="add">
             +
         </div>
+        <div class="form-add">
+            <form @submit.prevent="addUser">
+                <input type="text" placeholder="Nombre" v-model="nameAdd">
+                <input type="tel" placeholder="Numero" v-model="phoneAdd">
+                <input type="email" placeholder="Email" v-model="emailAdd">
+                <input type="text" placeholder="Contraseña" v-model="passwordAdd">
+                <button type="submit">Crear</button>
+            </form>
+        </div>
     </div>
 </template>
 <script setup>
-import {ref} from "vue";
+import { ref } from "vue";
 
-let users=ref([]);
+let users = ref([]);
+const nameAdd = ref("");
+const phoneAdd = ref("");
+const emailAdd = ref("");
+const passwordAdd = ref("");
+
 //Method to get all users to list
-const getUsers=async()=>{
-    const response=await fetch("http://localhost:3000/users");
-    const dataUsers=await response.json();
-    users.value=dataUsers;
+const getUsers = async () => {
+    const response = await fetch("http://localhost:3000/users");
+    const dataUsers = await response.json();
+    users.value = dataUsers;
 }
 
 getUsers();
 
+//Method to add user
+const addUser = async () => {
+    const newUser = {
+        name: nameAdd.value,
+        phone: phoneAdd.value,
+        email: emailAdd.value,
+        password: passwordAdd.value
+    };
+
+    const response = await fetch("http://localhost:3000/users/add",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newUser),
+        }
+    );
+
+    if (response.ok) {
+        console.log("Ok");
+        await getUsers();
+    } else {
+        console.log("ERROR");
+    }
+
+    nameAdd.value = "";
+    phoneAdd.value = "";
+    emailAdd.value = "";
+    passwordAdd.value = "";
+}
+
+//Method to delete user
+const deleteUser=async(id)=>{
+    const response = await fetch("http://localhost:3000/users/delete/"+id,
+        {
+            method: "DELETE"
+        }
+    );
+
+    if (response.ok) {
+        console.log("Ok");
+        await getUsers();
+    } else {
+        console.log("ERROR");
+    }
+
+}
+
+
 </script>
 <style scoped>
-*{
+* {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
 
 }
 
-div{
+div {
     display: flex;
     flex-direction: column;
     width: 100%;
@@ -70,7 +134,7 @@ div{
 
 }
 
-h2{
+h2 {
     display: block;
     width: 100%;
     height: 70px;
@@ -81,7 +145,7 @@ h2{
 
 }
 
-h3{
+h3 {
     display: block;
     width: 100%;
     height: 30px;
@@ -93,20 +157,23 @@ h3{
 
 }
 
-table, tr, td, th{
+table,
+tr,
+td,
+th {
     border: 1px solid rgba(0, 59, 103, 0.799);
 }
 
-th{
+th {
     color: rgb(0, 47, 81);
 }
 
-td{
+td {
     text-align: center;
     color: white;
 }
 
-.add{
+.add {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -122,6 +189,4 @@ td{
     right: 20px;
 
 }
-
-
 </style>
